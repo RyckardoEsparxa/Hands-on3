47ca9965a3a13a8abd782d6c697475bce36b8392
RyckardoEsparxa <95117423+RyckardoEsparxa@users.noreply.github.com>
2024-10-25 22:08:05 -0600

    Create helloworld.sh

---
+++ Hello World/helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Imprime "Hello World" en la terminal
+echo "Hello World"
+
+# Lista el contenido del directorio actual
+ls
+
+# Crea un directorio llamado Test
+mkdir Test
+
+# Cambia al directorio Test
+cd Test
+
+# Lista el contenido del directorio Test (que debería estar vacío en este punto)
+ls
